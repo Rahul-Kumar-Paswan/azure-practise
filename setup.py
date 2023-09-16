fde08a4a143d27937ba7929855f99912cff7cddd
Rahul-Kumar-Paswan <rahulkumarpaswan941+auropro@gmail.com>
2023-09-16 12:41:29 +0530

    adding main.py

--- setup.py
+++
@@ -1,8 +0,0 @@
-from setuptools import setup, find_packages
-
-setup(
-    install_requires=[
-        #"example_package==0.0.1",
-        "Flask",
-    ],
-)
--- src/main.py
+++ src/main.py
@@ -1,10 +1,10 @@
-from flask import Flask, render_template
+from flask import Flask
 
 app = Flask(__name__)
 
 @app.route('/')
 def index():
-    return 'Hello, Darling !!! Welcome to my World !!!'
+    return 'HAR HAR MAHADEV !!!'
 
 if __name__ == '__main__':
     app.run(host="0.0.0.0" , port=int("3000"),debug=True)
